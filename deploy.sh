abdec6bac225efbdb9599ef45df9910ddc085aea
Judy Fan <judy.fan@sap.com>
2021-11-19 17:58:11 +0800

    upgrade npm packages

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp _config.yml /githubP/blogP/
+cp _config.yml ~/githubP/blogP/
 cp package.json ~/githubP/blogP/
 cp node_modules/kramed/lib/rules/inline.js ~/githubP/blogP/node_modules/kramed/lib/rules/
 cp ./themes/hexo-theme-next/_config.yml ~/githubP/blogP/themes/hexo-theme-next/
