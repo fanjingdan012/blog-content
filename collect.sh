8e3cd68f434880b81e001bcf0dd875e8e459cd2d
Judy Fan <judy.fan@sap.com>
2021-11-19 17:06:50 +0800

    upgrade hexo

---
+++ collect.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cp ~/githubP/blogP/_config.yml .
+cp ~/githubP/blogP/package.json .
+cp ~/githubP/blogP/node_modules/kramed/lib/rules/inline.js ./node_modules/kramed/lib/rules/inline.js
+cp ~/githubP/blogP/themes/hexo-theme-next/_config.yml ./themes/hexo-theme-next/_config.yml
+cp -R ~/githubP/blogP/source .
