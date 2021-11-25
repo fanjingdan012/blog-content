1dd42407c2d11811632f45490df08607cba4fa02
fanjingdan <fanjingdan@sina.com>
2021-11-25 20:43:31 +0800

    update scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cp _config.yml ~/githubP/blogP/
-cp package.json ~/githubP/blogP/
+cp _config.yml ../blogP/
+cp package.json ../blogP/
 # cp node_modules/kramed/lib/rules/inline.js ~/githubP/blogP/node_modules/kramed/lib/rules/
-cp ./themes/hexo-theme-next/_config.yml ~/githubP/blogP/themes/hexo-theme-next/
-cp -R source ~/githubP/blogP/
+cp ./themes/hexo-theme-next/_config.yml ../blogP/themes/hexo-theme-next/
+cp -R source ../blogP/
